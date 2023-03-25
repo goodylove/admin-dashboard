824a9127f0eb50c1b23fbd072f8e974edf98adc0
goodness <goodyc474@gmail.com>
2023-03-25 16:10:12 +0100

    add styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+
+import SideBar from "./components/sidebarsection";
+import Body from "./components/bodysection";
 import "./App.css";
 
 function App() {
-  return <div className="App">APP</div>;
+  return (
+    <div className="container">
+      <SideBar />
+      <Body />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/sidebarsection/index.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Sidebar.css";
+
+const SideBar = () => {
+  return <div>SideBar</div>;
+};
+
+export default SideBar;
