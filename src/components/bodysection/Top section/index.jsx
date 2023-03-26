b8eb9ab9fed43e7c9530da6e90e264557cd1eda6
goodness <goodyc474@gmail.com>
2023-03-26 19:56:35 +0100

    complete top section

--- src/components/bodysection/Top section/index.jsx
+++ src/components/bodysection/Top section/index.jsx
@@ -1,7 +1,10 @@
 import React from "react";
-import { BiSearchAlt } from "react-icons/bi";
-import { TbMessageCircle } from "react-icons/tb";
 import AdminImage from "../../.././assets/source03.jpg";
+import VaseImage from "../../.././assets/main.png";
+import VideoImg from "../../.././assets/Grass - 66810.mp4";
+import { BiSearchAlt } from "react-icons/bi";
+import { BsArrowRightShort } from "react-icons/bs";
+import { TbMessageCircle } from "react-icons/tb";
 import { MdOutlineNotificationsNone } from "react-icons/md";
 import "./Top.css";
 
@@ -13,7 +16,7 @@ const Top = () => {
           <h1>Welcome to planti</h1>
           <p>Hello Goody,Welcome back!</p>
         </div>
-        <div className="searchBa flex">
+        <div className="searchBar flex">
           <input type="text" placeholder="Search Dashboard" />
           <BiSearchAlt className="icon" />
         </div>
@@ -23,6 +26,44 @@ const Top = () => {
 
           <div className="adminImage">
             <img src={AdminImage} alt="admin image" />
+          </div>
+        </div>
+      </div>
+      <div className="cardSection flex">
+        <div className="rightCard flex">
+          <h1>Create and sell extraordinary products</h1>
+          <p>The worls's fast growing industry are natural made products!</p>
+
+          <div className="buttons flex">
+            <button className="btn"> Explore more</button>
+            <button className="btn transparent"> Top Sellers</button>
+          </div>
+          <div className="videoDiv">
+            <video src={VideoImg} autoPlay loop muted></video>
+          </div>
+        </div>
+        <div className="leftCard flex">
+          <div className="main flex">
+            <div className="textDiv">
+              <h1> My Stat</h1>
+              <div className="flex">
+                <span>
+                  Today <br /> <small>4 Orders</small>
+                </span>
+              </div>
+              <div className="flex">
+                <span>
+                  This Month <br /> <small>127 Orders</small>
+                </span>
+              </div>
+              <span className="flex link">
+                Go to my orders
+                <BsArrowRightShort className="icon" />
+              </span>
+            </div>
+            <div className="imgDiv">
+              <img src={VaseImage} alt="" />
+            </div>
           </div>
         </div>
       </div>
