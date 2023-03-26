bff8c2a5bf2cac5ff93b29ae71a19b0c51b04e76
goodness <goodyc474@gmail.com>
2023-03-26 18:51:56 +0100

    add content to top section

--- src/components/bodysection/Top section/index.jsx
+++ src/components/bodysection/Top section/index.jsx
@@ -1,8 +1,33 @@
 import React from "react";
+import { BiSearchAlt } from "react-icons/bi";
+import { TbMessageCircle } from "react-icons/tb";
+import AdminImage from "../../.././assets/source03.jpg";
+import { MdOutlineNotificationsNone } from "react-icons/md";
 import "./Top.css";
 
 const Top = () => {
-  return <div>Top</div>;
+  return (
+    <div className="topSection">
+      <div className="headerSection flex">
+        <div className="title">
+          <h1>Welcome to planti</h1>
+          <p>Hello Goody,Welcome back!</p>
+        </div>
+        <div className="searchBa flex">
+          <input type="text" placeholder="Search Dashboard" />
+          <BiSearchAlt className="icon" />
+        </div>
+        <div className="adminDiv flex">
+          <TbMessageCircle className="icon" />
+          <MdOutlineNotificationsNone className="icon" />
+
+          <div className="adminImage">
+            <img src={AdminImage} alt="admin image" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Top;
